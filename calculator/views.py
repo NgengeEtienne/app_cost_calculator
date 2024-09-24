e36e43cf339cf36c633a3a5641cdcfab5ab81f18
ngengeetienne <ngengeetienne@gmail.com>
2024-09-24 10:22:50 -0500

    views

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Category, Feature
 
-# Create your views here.
+def calculate_cost(request):
+    if request.method == 'POST':
+        category_id = request.POST.get('category')
+        feature_ids = request.POST.getlist('features[]')
+        
+        # Calculate total hours based on selected features
+        total_hours = 0
+        for feature_id in feature_ids:
+            feature = Feature.objects.get(id=feature_id)
+            total_hours += feature.hours
+
+        total_cost = total_hours * 10  # $10/hour
+        return JsonResponse({'total_cost': total_cost})
