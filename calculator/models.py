9f63ad6f336f03ecb783c2f4fb0b0929e10b5ca4
ngengeetienne <ngengeetienne@gmail.com>
2024-09-24 10:21:41 -0500

    Models

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Feature(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='features')
+    name = models.CharField(max_length=100)
+    hours = models.IntegerField()
+
+    def __str__(self):
+        return self.name
